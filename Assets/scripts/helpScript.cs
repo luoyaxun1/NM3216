a65c4b1105fe5fa317186f6dcd415c5be24bbf01
a0129998 <a0129998@u.nus.edu>
2016-11-04 23:11:18 +0800

    added bounce sound, added help button, added non-ground rocks

---
+++ Assets/scripts/helpScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections;
+
+public class helpScript : MonoBehaviour {
+	public Button b;
+	public Image i;
+	// Use this for initialization
+	void Start () {
+		b.onClick.AddListener (ToggleImage);
+		i.enabled = false;
+	}
+
+
+	void ToggleImage(){
+		Debug.Log ("help clicked");
+		if (i.enabled) {
+			i.enabled = false;
+		} else {
+			i.enabled = true;
+		}
+	}
+
+}
